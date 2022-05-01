4a3bc0faee94548f611777cb621311eb9a29dbc7
Wellington <tinhoefaela@gmail.com>
2022-05-01 12:54:17 -0300

    create navbar

--- src/App.js
+++ src/App.js
@@ -6,20 +6,50 @@ import Fiados from './pages/GestaoDePedidos/Fiados/Fiados'
 import Login from './pages/Login/Login'
 import FiadosUser from './pages/GestaoDePedidos/Fiados/FiadoUser/FiadoUser'
 import CreateFiador from './pages/GestaoDePedidos/Fiados/createFiador/CreateFiador'
+import Navbar from './components/Navbar/Navbar';
 
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={ <Login />}  exact/>
-        <Route path='/gestao-de-pedidos' element={<Financeiro />} exact />
-        <Route path='/gestao-de-pedidos/fiados' element={<Fiados />} exact />
-        <Route path='/gestao-de-pedidos/fiados/createfiador' element={<CreateFiador />} exact />
 
-        <Route path='/gestao-de-pedidos/fiados/:id' element={<FiadosUser />} exact />
+        <Route path='/' element={<Login />} exact/>
 
-        <Route path='/gestao-de-pedidos/configuracoes' element={<Configuracoes />} exact />
+        <Route path='/gestao-de-pedidos' element={
+        <>
+        <Navbar />
+        <Financeiro />
+        </>} exact 
+        />
+
+        <Route path='/gestao-de-pedidos/fiados' element={
+        <>
+        <Navbar />
+        <Fiados />
+        </>} exact
+        />
+
+        <Route path='/gestao-de-pedidos/fiados/createfiador' element={
+        <>
+        <Navbar />
+        <CreateFiador />
+        </>} exact 
+        />
+
+        <Route path='/gestao-de-pedidos/fiados/:id' element={
+        <>
+        <Navbar />
+        <FiadosUser />
+        </>} exact
+        />
+
+        <Route path='/gestao-de-pedidos/configuracoes' element={
+        <>
+        <Navbar />
+        <Configuracoes />
+        </>} exact 
+        />
         
       </Routes>
     </Router>
--- src/pages/GestaoDePedidos/Configuracoes/Configuracoes.js
+++ src/pages/GestaoDePedidos/Configuracoes/Configuracoes.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import './configuracoes.css'
 
-import Sidebar from '../../../components/Sidebar/Sidebar'
-
 export default function Configuracoes() {
   return (
     <div className='configuracoes'>
-      <Sidebar />
     </div>
   )
 }
